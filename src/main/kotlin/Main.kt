afe44547a6060bd2d3fc595b86207554d1518b7d
ApofisGB <volkooff2000@gmail.com>
2022-11-08 21:49:47 +0300

    second commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,4 @@
+import kotlin.math.abs
 
 // Проверка строки на уникальность символов
 fun uniqueSymbols(string: String): Boolean {
@@ -5,7 +6,7 @@ fun uniqueSymbols(string: String): Boolean {
 
     val n = string.length - 1
     for (i in 0..n) {
-        if ((string[i] != ' ') and (string[i] in string.slice(i+1..n))) return false
+        if ((string[i] != ' ') and (string[i] in string.slice(i + 1..n))) return false
     }
 
     return true
@@ -37,10 +38,57 @@ fun replacingSpaces(string: String): String {
 }
 
 
+// Проверка на палиндром
+fun palindrome(string: String): Boolean {
+
+    val mapCounter = mutableMapOf<Char, Int>()
+
+    val newString = string.filter { !it.isWhitespace() }
+    for (i in newString.indices) {
+        mapCounter[newString[i]] = mapCounter.getOrDefault(newString[i], 0) + 1
+    }
+
+    var flag = false
+    for (value in mapCounter.values) {
+        if (value % 2 == 1) {
+            if (flag) return false
+            flag = true
+        }
+    }
+    return true
+}
+
+
+// Битовое расстояние Левенштейна
+fun minDistance(string1: String, string2: String): Boolean {
+
+    val n = string1.length
+    val m = string2.length
+    val diff = abs(n - m)
+    if (diff > 1) return false
+
+    var count = 0
+
+    var i = 0
+    var j = 0
+
+    while ((i < n - 1) and (j < m - 1)) {
+
+
+
+    }
+
+    count += diff
+
+    return (count < 2)
+}
+
+
 fun main() {
 
-    val string = readln()
-    print(replacingSpaces(string))
+    val string1 = readln()
+    val string2 = readln()
+    print(minDistance(string1, string2))
 
 }
 
